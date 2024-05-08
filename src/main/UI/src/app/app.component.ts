2dca8d07575a4ee31e3acf2527c7287ad490328c
Tcarri <133730561+Tcarri@users.noreply.github.com>
2024-05-08 14:15:41 -0700

    Everything is coming together at this point. At least on InteliJ, as for live not so much.

--- src/main/UI/src/app/app.component.ts
+++ src/main/UI/src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {AppModule} from "./app.module";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppModule],
+  imports: [RouterOutlet, AppModule, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Tami';
+  items = [
+    { title: 'Our Church', link: 'https://www.vvchurchlancaster.com/' },
+    { title: 'Donate here!', link: 'https://app.easytithe.com/inline/dedicated/?uid=vall4233225&amt=NaN' },
+    { title: 'Check out our Instagram', link: 'https://www.instagram.com/vvnaz_ma/' },
+    { title: 'World Belt Belt Bureau', link: 'https://wbbb.site/index.php' }
+  ];
 }
 
