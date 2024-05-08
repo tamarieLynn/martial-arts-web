bd01c2ef4fd65f0074176194336a6210e585acc1
Tcarri <133730561+Tcarri@users.noreply.github.com>
2024-05-08 15:45:25 -0700

    updating routing methods

--- src/main/UI/src/app/app.module.ts
+++ src/main/UI/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BackgroundComponent } from './background.component';
 import { CommonModule } from '@angular/common'; // Import CommonModule
